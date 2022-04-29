b0d78c2f0354130b1ecb9892b58f20656437a850
Achmad Rizky <rizflutter@gmail.com>
2022-04-29 15:39:58 +0700

    update dark mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,12 +4,21 @@ window.onscroll = function() {
 
     const fixedNav = header.offsetTop;
 
+    const toTop = document.querySelector('#to-top');
+
     // if scroll down
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        // back to top button
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     } else {
         // if scroll return to first state
         header.classList.remove('navbar-fixed');
+
+        // back to top button
+        toTop.classList.add('hidden');
+        toTop.classList.remove('flex');
     }
 }
 
@@ -22,3 +31,33 @@ hamburger.addEventListener('click', function() {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+
+// if click any area for close hamburger
+window.addEventListener('click', function(e) {
+    if(e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+})
+
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+});
+
+// move position toggle mode
+ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+} else {
+    darkToggle.checked = false;
+}
